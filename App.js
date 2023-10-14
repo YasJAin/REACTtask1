c0b74659589fb637444fff0321773a7a9d8ff831
Yash Jain <129750045+YasJAin@users.noreply.github.com>
2023-10-14 05:57:09 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { F1 } from './components/F1';
+
+function App() {
+  return (
+    <div>
+      <h2 style={{color:"red"}}>React-Task-1</h2>
+      <F1 />
+    </div>
+  );
+}
+
+export default App;
